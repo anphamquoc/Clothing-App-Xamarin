dfe01268c5d0a33fcf5496ce04be8f417fb0f2f3
tdb <tranducbao192@gmail.com>
2022-11-02 20:22:45 +0700

    update connect

--- ClothingApp/ClothingApp/Model/Connect.cs
+++ ClothingApp/ClothingApp/Model/Connect.cs
@@ -10,14 +10,14 @@ namespace ClothingApp.Model
     {
         private string GetConnectionString()
         {
-            EnvironmentVariable environmentVariable = new EnvironmentVariable();
-            string srvrdbname = environmentVariable.srvrdbname; // your database name
-            string srvrname = environmentVariable.srvrname; // your IPV4 address
-            string srvrusername = environmentVariable.srvrusername; // your username of SQL Server
-            string srvrpassword = environmentVariable.srvrpassword; // your password of SQL Server
-            string sqlconn = $"Data Source={srvrname};Initial Catalog={srvrdbname};User ID={srvrusername};Password={srvrpassword}";
-            return sqlconn;
-            //return "aaa";
+            //EnvironmentVariable environmentVariable = new EnvironmentVariable();
+            //string srvrdbname = environmentVariable.srvrdbname; // your database name
+            //string srvrname = environmentVariable.srvrname; // your IPV4 address
+            //string srvrusername = environmentVariable.srvrusername; // your username of SQL Server
+            //string srvrpassword = environmentVariable.srvrpassword; // your password of SQL Server
+            //string sqlconn = $"Data Source={srvrname};Initial Catalog={srvrdbname};User ID={srvrusername};Password={srvrpassword}";
+            //return sqlconn;
+            return "aaa";
         }
         public SqlConnection Connection
         {
