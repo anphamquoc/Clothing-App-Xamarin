f7ced0909a4d3256c8a4c58c118d2bc92e8bca42
tranducbao1 <19521257@gm.uit.edu.vn>
2022-11-15 21:51:02 +0700

    Add product and order pages

--- ClothingApp/ClothingApp/Views/Order.xaml.cs
+++ ClothingApp/ClothingApp/Views/Order.xaml.cs
@@ -7,29 +7,41 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using ClothingApp.Model;
+using ClothingApp.Interfaces;
+using ClothingApp.ViewModels;
+using ClothingApp.Helpers;
 
 namespace ClothingApp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Order : ContentPage
     {
+        CommonInterface CommonInterface = new CommonInterface();
         public Order()
         {
             InitializeComponent();
 
-            List<OrderItem> orderItems = new List<OrderItem> {
-                new OrderItem("Order 001", "28/07/2001", 2000, "Đang giao hàng"),
-                new OrderItem("Order 001", "28/07/2001", 2000, "Đang giao hàng"),
-                new OrderItem("Order 001", "28/07/2001", 2000, "Đang giao hàng"),
-                new OrderItem("Order 001", "28/07/2001", 2000, "Đang giao hàng")
-            };
+            //listItems.ItemsSource = orderItems;
+        }
 
-            listItems.ItemsSource = orderItems;
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            ApiResponse apiResponse = await CommonInterface._orderRepository.GetAllOrderByUserId(((App)App.Current).UserId);
+            if (apiResponse.success == false)
+            {
+                await DisplayAlert("Thông báo", apiResponse.message, "OK");
+                return;
+            }
+
+            List<OrderVM> orderVMs = new ConvertObject<OrderVM>().ConvertObjectToListData(apiResponse.data);
+            listItems.ItemsSource = orderVMs;
         }
 
         private async void listItems_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
-            await Shell.Current.GoToAsync("//DetailOrder");
+            OrderVM orderVM = listItems.SelectedItem as OrderVM;
+            await Shell.Current.GoToAsync($"//DetailOrder?OrderId={orderVM.id}");
         }
     }
 }
--- ClothingApp/ClothingApp/Views/ProductInformation.xaml.cs
+++ ClothingApp/ClothingApp/Views/ProductInformation.xaml.cs
@@ -1,25 +1,87 @@
-﻿using System;
+﻿using ClothingApp.Helpers;
+using ClothingApp.Interfaces;
+using ClothingApp.Model;
+using ClothingApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Web;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace ClothingApp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ProductInformation : ContentPage
+    public partial class ProductInformation : ContentPage, IQueryAttributable
     {
+        readonly CommonInterface CommonInterface = new CommonInterface();
+        public string ProductId { get; set; }
         public ProductInformation()
         {
             InitializeComponent();
         }
 
+        public void ApplyQueryAttributes(IDictionary<string, string> query)
+        {
+            this.ProductId = query["productId"];
+        }
+
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await Task.Yield();
+            ApiResponse apiResponse = await CommonInterface._productRepository.GetProductById(this.ProductId);
+
+            if (!apiResponse.success)
+            {
+                await DisplayAlert("Thông báo", apiResponse.message, "OK");
+                await Shell.Current.GoToAsync("//Home");
+            }
+
+            ProductVM productVM = new ConvertObject<ProductVM>().ConvertObjectToData(apiResponse.data);
+
+            titlePage.Text = productVM.Name;
+            name.Text = productVM.Name;
+            description.Text = productVM.Description;
+            image.Source = productVM.ImgUrl;
+            price.Text = new ConvertToCurrencyFormat(productVM.Price.ToString()).Result;
+
+        }
+
+        
+
         private async void BackToHome_btn_Clicked(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync("//Home");
+        }
+
+        private async void AddToCart_btn_Clicked(object sender, EventArgs e)
+        {
+            string cartId = ((App)App.Current).CartId;
+            AddToCartModel addToCartModel = new AddToCartModel
+            {
+                CartId = cartId,
+                ProductId = ProductId
+            };
+
+            ApiResponse apiResponse = await CommonInterface._cartDetailRepository.AddProductToCart(addToCartModel);
+
+            if (!apiResponse.success)
+            {
+                await DisplayAlert("Thông báo", apiResponse.message, "OK");
+            }
+
+            else await DisplayAlert("Thông báo", "Thêm vào giỏ hàng thành công", "OK");
+
+            return;
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//Shopping");
         }
     }
 }
