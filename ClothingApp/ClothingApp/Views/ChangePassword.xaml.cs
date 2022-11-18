3bfaab0a9fe4f0ed238458fe56376490e0e3cf98
tranducbao1 <19521257@gm.uit.edu.vn>
2022-11-18 11:16:14 +0700

    Add change password page

--- ClothingApp/ClothingApp/Views/ChangePassword.xaml.cs
+++ ClothingApp/ClothingApp/Views/ChangePassword.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ClothingApp.Helpers;
+using ClothingApp.Interfaces;
+using ClothingApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +15,7 @@ namespace ClothingApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ChangePassword : ContentPage
     {
+        public CommonInterface CommonInterface = new CommonInterface();
         public ChangePassword()
         {
             InitializeComponent();
@@ -19,11 +23,40 @@ namespace ClothingApp.Views
 
         private async void Confirm_Clicked(object sender, EventArgs e)
         {
+            string Password = password.Text;
+            string NewPassword = newPassword.Text;
+            string ReNewPassword = reNewPassword.Text;
+
+            if (NewPassword != ReNewPassword)
+            {
+                await DisplayAlert("Thông báo", "Mật khẩu mới không khớp với trường nhập lại mật khẩu mới", "OK");
+                return;
+            }
+
+            ChangeUserPasswordModel changeUserPasswordModel = new ChangeUserPasswordModel
+            {
+                Password = Password,
+                NewPassword = NewPassword
+            };
+
+            ApiResponse apiResponse = await CommonInterface._userRepository.ChangeUserPassword(((App)App.Current).UserId, changeUserPasswordModel);
+
+            if (apiResponse.success == false)
+            {
+                await DisplayAlert("Thông báo", apiResponse.message, "OK");
+                return;
+            }
+
             await DisplayAlert("Thông báo", "Đổi mật khẩu thành công", "OK");
             await Shell.Current.GoToAsync("//User");
         }
 
         private async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//User");
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync("//User");
         }
